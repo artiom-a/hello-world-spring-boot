9a1eb32a7347a50e0b2c6670ce8e2309c62e3565
Shubhangi Thakur <113595617+shubhangithakur-cloudside@users.noreply.github.com>
2023-04-20 16:47:45 +0530

    Update HelloMessage.java

--- springapp/src/main/java/com/example/springapp1/controller/HelloMessage.java
+++ springapp/src/main/java/com/example/springapp1/controller/HelloMessage.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 // }
 
 @RestController
-public class CommentStoreApp {
+public class HelloMessage {
 
     @RequestMapping("/") 
     public String hello() {
